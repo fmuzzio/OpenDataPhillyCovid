283eb753c061f132cac92f7a46c9a6b2dad91a0d
fmuzzio <fmuzzio@seas.upenn.edu>
2023-04-10 12:57:34 -0700

    New classes

--- src/edu/upenn/cit5940/Main.java
+++ src/edu/upenn/cit5940/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Check if the correct number of runtime arguments were passed
+		
+		if (args.length != 4) {
+	        System.out.println("Error in runtime arguments, set run Configuration arguments in form: <covid_filename> <properties_filename> <population_filename> <log_filename>");
+	        return;
+	    }
+	    
+	    // Initialize filenames
+	    String covidFilename = args[0];
+	    String propertiesFilename = args[1];
+	    String populationFilename = args[3];
+	    String logFilename = args[4];
 
 	}
 
--- src/edu/upenn/cit5940/logging/Logger.java
+++ src/edu/upenn/cit5940/logging/Logger.java
@@ -1,5 +1,71 @@
 package edu.upenn.cit5940.logging;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 public class Logger {
 
+    private static Logger instance;
+    private String logFileName;
+    private Writer logFileWriter;
+
+    private Logger() {}
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public synchronized void log(String message) {
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String logMessage = timestamp + " " + message + "\n";
+        try {
+            if (logFileName != null) {
+                if (logFileWriter == null) {
+                    logFileWriter = new FileWriter(logFileName, true);
+                }
+                logFileWriter.write(logMessage);
+                logFileWriter.flush();
+            } else {
+                System.err.print(logMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void setOutput(String fileName) {
+        if (fileName == null) {
+            logFileName = null;
+            if (logFileWriter != null) {
+                try {
+                    logFileWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                logFileWriter = null;
+            }
+        } else {
+            if (!fileName.equals(logFileName)) {
+                if (logFileWriter != null) {
+                    try {
+                        logFileWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    logFileWriter = new FileWriter(fileName, true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                logFileName = fileName;
+            }
+        }
+    }
 }
