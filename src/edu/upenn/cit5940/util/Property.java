67325a09fb42c8a117d3cb69581dea17be2ee4bf
Leo Li <64953669+Yuxing-Li@users.noreply.github.com>
2023-04-11 15:44:43 +0800

    Update Property.java

--- src/edu/upenn/cit5940/util/Property.java
+++ src/edu/upenn/cit5940/util/Property.java
@@ -1,1 +1,43 @@
+package edu.upenn.cit594.util;
 
+public class Property {
+	
+	private String zip_code;
+	private double market_value = 0;
+	private double total_livable_area = 0;
+	
+	public Property(String zip_code, double market_value, double total_livable_area) {
+		this.zip_code = zip_code;
+		this.market_value = market_value;
+		this.total_livable_area = total_livable_area;
+	}
+
+
+
+	public String getZip_code() {
+		return zip_code;
+	}
+
+	public void setZip_code(String zip_code) {
+		this.zip_code = zip_code;
+	}
+
+	public double getMarket_value() {
+		return market_value;
+	}
+
+	public void setMarket_value(int market_value) {
+		this.market_value = market_value;
+	}
+
+	public double getTotal_livable_area() {
+		return total_livable_area;
+	}
+
+	public void setTotal_livable_area(int total_livable_area) {
+		this.total_livable_area = total_livable_area;
+	}
+
+	
+
+}
