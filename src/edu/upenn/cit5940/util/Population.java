313fcc5a2842c76d78b338ebeafbf84e1098c76f
Leo Li <64953669+Yuxing-Li@users.noreply.github.com>
2023-04-11 15:23:20 +0800

    Update Population.java

--- src/edu/upenn/cit5940/util/Population.java
+++ src/edu/upenn/cit5940/util/Population.java
@@ -1,1 +1,29 @@
+package edu.upenn.cit594.util;
 
+public class Population {
+	
+	private String zipCode;
+	private int population;
+	
+	public Population (String zipCode, int population) {
+		this.population = population;
+		this.zipCode = zipCode;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+  
+  public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+}
